d2d7c4491648b08a42d688ca86d0c6ecee1fe548
JAIME_CARRIEL <jaimecarriel302@gmail.com>
2025-07-23 23:33:08 -0500

    connection_fixed_retry_timeout

--- producer/producer.py
+++ producer/producer.py
@@ -1,16 +1,35 @@
 import json, time
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
-channel = connection.channel()
 
-channel.queue_declare(queue="IoT_Tasks", durable=True)
+def connect_rabbitmq():
+    for i in range(20):
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+            return connection
+        except:
+            print("Reconexion con RABBITMQ")
+            time.sleep(3)
+    raise Exception("Conexion Fallida")
+
 
 def main():
-    channel.basic_publish(exchange='',
-                          routing_key='IoT_Tasks', body="Update Metrics", 
+    connection = connect_rabbitmq()
+    channel = connection.channel()
+    channel.queue_declare(queue="IoT_Tasks", durable=True)
+    sensors = ["sensor-01","sensor-2","sensor-03"]
+    tasks = ["Process Images","Update firmware","Process metrics"]
+    for i in range(3):
+         task = {
+             'id' : i+1,
+             'task': tasks[i],
+             'producer': sensors[i]
+         }
+         channel.basic_publish(exchange='',
+                          routing_key='IoT_Tasks', body=json.dumps(task), 
                           properties=pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent))
-
+         time.sleep(5)
+         print(task)
     connection.close()
 
 if __name__ == '__main__':
