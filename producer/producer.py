3c1d7d7af095935450ecbebfdfc9157dff082090
JAIME_CARRIEL <jaimecarriel302@gmail.com>
2025-07-29 23:27:02 -0500

    Comments Added

--- producer/producer.py
+++ producer/producer.py
@@ -1,36 +1,53 @@
-import json, time
-import pika
+#Dependencias
+import json #Libreria para el manejo de mensajes en formato JSON
+import pika #Libreria para el uso de la cola de mensajes RabbitMQ
+import time #Libreria para realizar delays
 
 
+#Funcion para conectar el producer con el RabbitMQ
 def connect_rabbitmq():
+    #Realiza 20 intentos
     for i in range(20):
         try:
-            connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+            print(f"[PRODUCER] Intentando conectar a RabbitMQ (intento {i+1})...", flush=True)
+            connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq')) #Crea la conexion con el RabbitMQ
+            # Si la conexión es exitosa, retornar el objeto connection
             return connection
-        except:
-            print("Reconexion con RABBITMQ")
-            time.sleep(3)
-    raise Exception("Conexion Fallida")
+                #Si hubo un error, imprime un mensaje en consola indicando que hubo un error al conectar en conjunto con el detalle error que hubo
+        except Exception as e:
+            print(f"[PRODUCER] Error al conectar:{e}. Reintentando nuevamente",flush=True)
+            time.sleep(3) # Inicializa un delay de 3 segundos al programa
+    #Si dentro de los 20 intentos no hubo ningun exito de comunicacion, envia error de Conexion Fallida
+    raise Exception("[PRODUCER] Conexion Fallida")
 
-
+#Funcion Principal del productor 
 def main():
-    connection = connect_rabbitmq()
-    channel = connection.channel()
-    channel.queue_declare(queue="IoT_Tasks", durable=True)
-    sensors = ["sensor-01","sensor-2","sensor-03"]
-    tasks = ["Process Images","Update firmware","Process metrics"]
-    for i in range(3):
-         task = {
-             'id' : i+1,
-             'task': tasks[i],
-             'producer': sensors[i]
-         }
-         channel.basic_publish(exchange='',
-                          routing_key='IoT_Tasks', body=json.dumps(task), 
-                          properties=pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent))
-         time.sleep(5)
-         print(task)
-    connection.close()
+    connection = connect_rabbitmq() # Establece la comunicacion con RabbitMQ
+    channel = connection.channel() # Crea un canal de comunicacion dentro de la conexion
+    channel.queue_declare(queue="IoT_Tasks", durable=True) # Declara una cola de mensajes IoT_Tasks dentro del canal de comunicacion
+    sensors = ["sensor-01","sensor-2","sensor-03"] # Lista para la simulacion de sensores
+    tasks = ["Procesar Imagenes","Actualizar firmware","Analizar metricas"] # Posibles tareas que puede mandar cada sensor
+    # Bucle para mandar de manera constante tareas
+    num_task = 0 
+    while True:
+            #Va a enviar de cada sensor una tarea diferent
+            for i in range(3):
+                # Crea un diccionario de datos que va a contener los datos de la tarea
+                num_task+=1 # Indica que numero de tarea es
+                task = {
+                'id' : num_task, # ID de la tarea
+                'task': tasks[i], # Tarea
+                'producer': sensors[i] # Sensor que la mando a la cola de mensajes
+                }
+                #Publica mensajes dentro de la cola
+                channel.basic_publish(exchange='', # Usa un exchange directo
+                          routing_key='IoT_Tasks', # Nombre de la cola destino
+                          body=json.dumps(task), # Transforma el diccionario de datos a formato Json
+                          properties=pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent)) # Indicar que los mensajes sean persitentes
+                time.sleep(5)
+                # Espera despues de cada 5 segundo para mandar el otro mensaje
+                print(task,flush=True)
+
 
 if __name__ == '__main__':
     main()
--- worker/worker.py
+++ worker/worker.py
@@ -1,43 +1,56 @@
-import json
-import pika
-import time
+#Dependencias
+import json #Libreria para el manejo de mensajes en formato JSON
+import pika #Libreria para el uso de la cola de mensajes RabbitMQ
+import time #Libreria para realizar delays
 
+#Funcion para conectar el worker con el RabbitMQ
 def connect_rabbitmq():
-    for i in range(20):
+    for i in range(20): #Realiza 20 intentos 
+        #Intenta realizar la conexion
         try:
             print(f"[WORKER] Intentando conectar a RabbitMQ (intento {i+1})...", flush=True)
-            connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
-            print("[WORKER] Conectado a RabbitMQ", flush=True)
-            return connection
+            connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq')) #Crea la conexion con el RabbitMQ
+            # Si la conexión es exitosa, retornar la el objeto que representa la conexion
+            return connection 
+        #Si hubo un error, imprime un mensaje en consola indicando que hubo un error al conectar en conjunto con el detalle error que hubo
         except Exception as e:
-            print(f"[WORKER] Error al conectar: {e}", flush=True)
-            time.sleep(3)
-    raise Exception("No se pudo conectar a RabbitMQ")
+            print(f"[WORKER] Error al conectar:{e}. Reintentando nuevamente", flush=True)
+            time.sleep(3) #Inicia un delay de 3 segundos
+    #Si despues de los 20 intentos, levanta un error indicando que RabbitMQ no pudo conectarse
+    raise Exception("[WORKER] Conexion fallida")
 
+#Funcion la cual se va a ejecutar cuando el worker recibe un mensaje en cola
 def callback(ch, method, properties, body):
-    task = json.loads(body)
-    print(f"[WORKER] Recibido: {task}", flush=True)
+    task = json.loads(body) # Carga el mensaje JSON y lo transforma en un diccionario de datos en PYTHON
+    print(f"[WORKER] Recibido: {task}", flush=True) # Imprime el diccionario de datos recibido
     print(f"[WORKER] Procesando tarea '{task['task']}' del productor {task['producer']}...", flush=True)
-    time.sleep(3)
+    time.sleep(3) #Realiza un delay simulando el procesamiento de una tarea
     print(f"[WORKER] Tarea {task['id']} completada", flush=True)
-    ch.basic_ack(delivery_tag=method.delivery_tag)
+    ch.basic_ack(delivery_tag=method.delivery_tag) #Envia un acknowledgment confirmando que el mensaje fue recibido y procesado
 
+# Funcion Principal del worker
 def main():
     print("[WORKER] Iniciando...", flush=True)
-    connection = connect_rabbitmq()
-    channel = connection.channel()
-    channel.queue_declare(queue="IoT_Tasks", durable=True)
-    channel.basic_qos(prefetch_count=1)
-    channel.basic_consume(queue="IoT_Tasks", on_message_callback=callback)
+    connection = connect_rabbitmq() # Establece e inicializa la conexion con RabbitMQ
+    channel = connection.channel() # Crea un canal de comunicacion dentro de la conexion
+    channel.queue_declare(queue="IoT_Tasks", durable=True) # Declara una cola de mensajes, en este caso, IoT_Tasks e indica que los mensajes dentro de la cola sea durable 
+    channel.basic_qos(prefetch_count=1) # Configuracion qos para indicar que el worker solo puede procesar un mensaje a la vez
+    channel.basic_consume(queue="IoT_Tasks", on_message_callback=callback) # Indica que funcion se debe ejecutar para procesar mensajes, cuando reciba mensajes de la cola de mensajes IoT_Tasks
 
     print("[WORKER] Esperando tareas...", flush=True)
     try:
+        #Inicia una bucle de manera indefinida para el consumo de mensajes
         channel.start_consuming()
-    except KeyboardInterrupt:
-        print("[WORKER] Detenido manualmente", flush=True)
+    #Si se da una error, detiene el consumo de mensajes y la comunicacion
+    except Exception as e:
+        print(f"[WORKER] Detenido por {e}", flush=True)
+        #Indica que debe detener el bucle, por lo tanto, el consumo de mensaje
+        channel.stop_consuming()
+    finally:
+        #Cierra el canal de comunicacion
+        channel.close()
 
 
 if __name__ == "__main__":
-    print("=== WORKER ARRANCANDO ===", flush=True)
     main()
     
